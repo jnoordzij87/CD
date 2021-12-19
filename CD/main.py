0b73fc5dbbaf99d6444a475e12ca3e0012ed9c41
Jos Noordzij <Jos.Noordzij@demobv.nl>
2021-12-20 00:07:47 +0100

    Change comment

--- CD/main.py
+++ CD/main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 from utils.taskwindow import TaskWindow
 
 # entry point of the application
-# create a ui window for checking tasks and setting sources 
+# creates a ui window for checking tasks and setting sources 
 # further actions arise from button clicks in that ui window
 
 root = Tk()
