7af18273c000a3ec5297969466331df861c24eb4
Jos Noordzij <Jos.Noordzij@demobv.nl>
2021-12-20 21:24:16 +0100

    Cleanup

--- CD/utils/updater.py
+++ CD/utils/updater.py
@@ -14,41 +14,24 @@ from utils.zipBuilder import *
 class Updater:
 
     def Update(self, tasks):
-        
         self.StartTime = self.GetTime()
         self.Tasks = tasks
-        
         #stop if no working vpn connection
         if not self.TestVPN():
             return
-        
         #create ZIP files 
         zipBuilderObj = ZipBuilder(tasks)
         zipBuilderObj.CreateZipFiles()
-        
         #execute task on server 
         for task in self.Tasks:
            task = self.ExecuteTaskOnServer(taskId)
-
         print('All tasks completed.')
-    
-    def TestVPN(self):
-        print('Testing VPN...')
-        if not os.path.exists(r"\\demo-ts-2\DEMO\Demonstratie"):
-            print('...VPN not working, aborted!')
-            return False
-        else:
-            print('...working.')
-            return True
 
     def ExecuteTaskOnServer(self, task):
-
         print('Starting distribution for', task.Program, task.Environment)
-
         #get updatefolderpath and zipfilepath from timestamp
         task.UpdateFolderPath = task.GetUpdateFolderPath(timeSuffix)
         task.ZipFileCopyDstPath = os.path.join(updateFolder, os.path.basename(task.ZipFileCopySrcPath))
-
         #copy zip to server, but only once per program/version/server
         #i.e. if 2 envs on 1 server need the same binaries, copy zip once and use for both envs
         copiedBefore, fileToUnzip = self.HasProgramBeenCopiedToServer(task)
@@ -61,18 +44,14 @@ class Updater:
             copySuccess = self.CopyZipToServer(task)
             #proceed with unzip if copy successful
             doUnzip = copySuccess
-            
         #unzip on server
         if doUnzip:
             unzipperObj = Unzipper()
             unzipperObj.UnzipOnServer(task)
-
-            #TODO NEXT:
-            #unzip into live folder
-            #print('Proceeding to unzip in update folder: ', environment, program)
-            #self.Unpack(fileToUnzip, liveFolder)
         
+        print("-----------------")
         print("---END OF TASK---")
+        print("-----------------")
 
     def HasProgramBeenCopiedToServer(self, thistask):
         for task in self.Tasks:
@@ -107,7 +86,6 @@ class Updater:
             print('ERROR ----- Exception while copying ZIP to update folder. Be sure to check that your VPN is working correctly. Exception message: ' + str(e))
         if success:
             print('...complete. ')
-
         return success
 
     def GetTime(self):
@@ -116,5 +94,14 @@ class Updater:
         result = date+"_"+time
         print(result)
         return result
+
+    def TestVPN(self):
+        print('Testing VPN...')
+        if not os.path.exists(r"\\demo-ts-2\DEMO\Demonstratie"):
+            print('...VPN not working, aborted!')
+            return False
+        else:
+            print('...working.')
+            return True
         
     
