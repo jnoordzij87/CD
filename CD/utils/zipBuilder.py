d4f6e1051777c1c66464de060da291725d312a99
Jos Noordzij <Jos.Noordzij@demobv.nl>
2021-12-20 21:20:45 +0100

    Remove unnecessary lookups

--- CD/utils/zipBuilder.py
+++ CD/utils/zipBuilder.py
@@ -5,7 +5,51 @@ from zipfile import ZipFile
 import zipfile
 import glob
 from shutil import *
-import utils.zipFilters
+from utils.zipFilters import *
 
-class ZipBuilder:    
-    pass
+class ZipBuilder:
+    
+    def __init__(self, tasks):
+        self.Tasks = tasks
+    
+    def CreateZipFiles(self):
+        for task in self.Tasks:
+            isZippingNecessary, zipSrcPath = self.IsMakingZipFileNecessary(task)
+            if isZippingNecessary:
+                binarysource = task.BinSourcePath
+                #get files to zip
+                filesToZip = FilterDir(task)
+                #create zip
+                zipFile = self.CreateZipFile(task, filesToZip)
+                #register that zip has been made for this task
+                task.HasACreatedZipfile = True
+                #store reference to zipfile location
+                task.ZipFileCopySrcPath = zipFile.filename
+            else:
+                #store reference to zipfile location
+                task.ZipFileCopySrcPath = zipSrcPath
+                    
+            
+    def CreateZipFile(self, task, filesToZip):
+        print("Building ZIP for {} {} ...".format(task.Program, task.Version))
+        zipfilepath = os.path.join(task.BinSourcePath, (self.StartTime + ".zip"))
+        zf = ZipFile(zipfilepath, 'w')
+        for file in filesToZip:
+            #get archivename (i.e. filepath without zip root)
+            archivename = file.split(sourcepath)[-1]
+            #add to zip 
+            zf.write(file, archivename) 
+        zf.close()
+        print('...complete')
+        return zf
+
+    def IsMakingZipFileNecessary(self, thistask):
+        #only necessary if there is not already a task that has created a zipfile for the same program and version  
+        for task in self.Tasks:
+            sameProgram = task.Program == thistask.Program
+            sameVersion = task.Version == thistask.Verion
+            if task.HasACreatedZipfile and sameProgram and sameVersion:
+                #zip already available
+                return False, task.ZipFileCopySrcPath
+        #if we are here, zip doesnt exist yet
+        return True, None
